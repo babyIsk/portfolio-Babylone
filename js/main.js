822301834306f3b12fe119cb50a2f0584ced41f0
babyIsk <babylone.isshak@gmail.com>
2026-02-25 16:55:53 +0100

    fix bug traduction

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import './modules/navbar.js';
-import './modules/projects.js';
-import './modules/animations.js';
-import './modules/langues.js';
-
-import { updateLang } from './modules/langues.js';
-updateLang('french');
---
+++ main.js
@@ -0,0 +1,7 @@
+import './js/modules/navbar.js';
+import './js/modules/projects.js';
+import './js/modules/animations.js';
+import './js/modules/langues.js';
+
+import { updateLang } from './js/modules/langues.js';
+updateLang('french');
